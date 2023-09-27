b1cfc9bceddcc8635e5c956900b405badfc17260
camipaula <camilapaulavegatandazo29@gmail.com>
2023-09-27 11:18:33 -0500

    Agregar archivos de proyecto.

---
+++ Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUDmvc.Models
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set;}
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+
+    }
+}
---
+++ Util/Utils.cs
@@ -0,0 +1,37 @@
+﻿using CRUDmvc.Models;
+namespace CRUDmvc.Util
+
+{
+    public class Utils
+    {
+        public static List<Producto> ListaProductos = new List<Producto>()
+        {
+            new Producto()
+            {
+                IdProducto = 1,
+                Nombre = "Producto1",
+                Descripcion="Descripcion 1",
+                Cantidad = 1
+            },
+
+            new Producto()
+            {
+                IdProducto = 2,
+                Nombre = "Producto2",
+                Descripcion="Descripcion 2",
+                Cantidad = 1
+            },
+            new Producto()
+            {
+                IdProducto = 3,
+                Nombre = "Producto3",
+                Descripcion="Descripcion 3",
+                Cantidad = 1
+            },
+
+
+
+
+        };
+    }
+}
