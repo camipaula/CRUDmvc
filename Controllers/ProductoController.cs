19438c760e852d8d50378acf1c400c9b83b322c9
camipaula <camilapaulavegatandazo29@gmail.com>
2023-10-10 16:17:10 -0500

    Clase 2

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CRUDmvc.Models;
+using CRUDmvc.Util;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CRUDmvc.Controllers
@@ -6,37 +8,70 @@ namespace CRUDmvc.Controllers
     public class ProductoController : Controller
     {
         // GET: ProductoController
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View(Util.Utils.ListaProductos);
         }
 
         // GET: ProductoController/Details/5
-        public ActionResult Details(int id)
+        public IActionResult Details(int id)
         {
             return View();
         }
 
         // GET: ProductoController/Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
 
-        
+        [HttpPost]
+        public IActionResult Create(Producto producto)
+        {
+            Console.WriteLine(producto.Nombre);
+            int id = Utils.ListaProductos.Count() + 1;
+            Utils.ListaProductos.Add(producto);
+            return RedirectToAction("Index");
+        }
+
 
         // GET: ProductoController/Edit/5
-        public ActionResult Edit(int id)
+        public IActionResult Edit(int IdProducto)
         {
+            Producto producto = Utils.ListaProductos.Find(x => x.IdProducto == IdProducto);
+            if (producto != null)
+            {
+                return View(producto);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Producto producto)
+        {
+            Producto producto2 = Utils.ListaProductos.Find(x => x.IdProducto == producto.IdProducto);
+            if (producto != null)
+            {
+                producto2.Nombre = producto.Nombre;
+                producto2.Descripcion = producto.Descripcion;
+                producto2.Cantidad = producto.Cantidad;
+                return RedirectToAction("Index");
+                
+            }
             return View();
         }
 
-       
+
 
         // GET: ProductoController/Delete/5
-        public ActionResult Delete(int id)
+        public IActionResult Delete(int IdProducto)
         {
-            return View();
+            Producto producto = Utils.ListaProductos.Find(x => x.IdProducto==IdProducto);
+            if (producto != null)
+            {
+                Utils.ListaProductos.Remove(producto);
+            }
+            return RedirectToAction("Index");
         }
 
       
--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,11 +1,17 @@
-﻿namespace CRUDmvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDmvc.Models
 {
     public class Producto
     {
         public int IdProducto { get; set; }
+        [Required]
         public string Nombre { get; set;}
+       
         public string Descripcion { get; set; }
+        [Required]
         public int Cantidad { get; set; }
+       
 
     }
 }
